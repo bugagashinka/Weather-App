5b3616c3df18811d25edc23f9bfe710437162dd6
bugagashinka <Voloshyn.Andrew@gmail.com>
2019-03-02 12:45:28 +0200

    Initial Weather-App project

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+(function() {
+  const STORAGE = window.localStorage;
+  const STORAGE_IMG_KEY = 'lastImg';
+  const IMG_ARR = ['kyiv,ua', 'florida,usa', 'colorado,usa', 'nyc,usa'];
+
+  const target = document.getElementsByClassName('target')[0];
+  const bgImage = document.getElementsByClassName('bg-image')[0];
+
+  target.addEventListener('mousemove', e => {
+    let moveX = (e.pageX * -1) / 20;
+    let moveY = (e.pageY * -1) / 20;
+    e.target.style.backgroundPosition = `${moveX}px ${moveY}px`;
+  });
+
+  const shuffle = arr => {
+    return arr.sort(function() {
+      return 0.5 - Math.random();
+    });
+  };
+
+  const randImgArr = shuffle(IMG_ARR);
+  const lastImg = STORAGE.getItem(STORAGE_IMG_KEY);
+  const randImg = lastImg === randImgArr[0] ? randImgArr[1] : randImgArr[0];
+
+  bgImage.style.backgroundImage = `url('./img/${randImg}.jpg')`;
+  target.style.backgroundImage = `url('./img/${randImg}.jpg')`;
+  STORAGE.setItem(STORAGE_IMG_KEY, randImg);
+
+})();
