f9b07b4d4d5ed8b21465f838d12224368767e58d
bugagashinka <Voloshyn.Andrew@gmail.com>
2019-03-06 17:40:08 +0200

    Change View

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,34 @@
-  import {App} from './components/App/';
-  import {Temperature} from '../'
+  // import {App} from './components/App/';
+  // import {Temperature} from '../';
 
-  new App(document.getElementById('app'));
+  // new App(document.getElementById('app'));
 
-  // const STORAGE = window.localStorage;
-  // const STORAGE_IMG_KEY = 'lastImg';
-  // const IMG_ARR = ['kyiv,ua', 'florida,usa', 'colorado,usa', 'nyc,usa'];
+  const container = document.getElementsByClassName('container')[0];
+  const target = document.getElementsByClassName('target')[0];
+  const body = document.getElementsByTagName('body')[0];
+  const bgImage = document.getElementsByClassName('bg-image')[0];
 
-  // const target = document.getElementsByClassName('target')[0];
-  // const bgImage = document.getElementsByClassName('bg-image')[0];
+  body.addEventListener('mousemove', e => {
+    let moveX = (e.pageX * -1) / 25;
+    let moveY = (e.pageY * -1) / 25;
+    target.style.backgroundPosition = `${moveX}px ${moveY}px`;
+  });
 
-  // target.addEventListener('mousemove', e => {
-  //   let moveX = (e.pageX * -1) / 20;
-  //   let moveY = (e.pageY * -1) / 20;
-  //   e.target.style.backgroundPosition = `${moveX}px ${moveY}px`;
-  // });
+  const STORAGE_IMG_KEY = 'lastImg';
+  const storage = window.localStorage;
 
-  // const shuffle = arr => {
-  //   return arr.sort(function() {
-  //     return 0.5 - Math.random();
-  //   });
-  // };
+  const imgArr = ['kyiv,ua', 'florida,usa', 'colorado,usa', 'nyc,usa'];
 
-  // const randImgArr = shuffle(IMG_ARR);
-  // const lastImg = STORAGE.getItem(STORAGE_IMG_KEY);
-  // const randImg = lastImg === randImgArr[0] ? randImgArr[1] : randImgArr[0];
+  const shuffle = arr => {
+    return arr.sort(function () {
+      return 0.5 - Math.random();
+    });
+  };
 
-  // bgImage.style.backgroundImage = `url('./img/${randImg}.jpg')`;
-  // target.style.backgroundImage = `url('./img/${randImg}.jpg')`;
-  // STORAGE.setItem(STORAGE_IMG_KEY, randImg);
+  const randImgArr = shuffle(imgArr);
+  const lastImg = storage.getItem(STORAGE_IMG_KEY);
+  const randImg = lastImg === randImgArr[0] ? randImgArr[1] : randImgArr[0];
+  bgImage.style.backgroundImage = `url('./assets/img/${randImg}.jpg')`;
+  target.style.backgroundImage = `url('./assets/img/${randImg}.jpg')`;
+  storage.setItem(STORAGE_IMG_KEY, randImg);
 
