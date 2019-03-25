3788b236395df05a095ff8fcf05def75c44bf17c
bugagashinka <Voloshyn.Andrew@gmail.com>
2019-03-25 22:40:23 +0200

    Add dummy type-handler for SearchBar

--- src/js/components/WeatherTarget/SearchBar/SearchBar.js
+++ src/js/components/WeatherTarget/SearchBar/SearchBar.js
@@ -1,11 +1,26 @@
 import Component from "../../../framework/Component";
 
+const ENTER_KEY = 13;
 export default class SearchBar extends Component {
   constructor(host, props) {
     super(host, props);
   }
 
+  keyPressHandler(e) {
+    if (e.keyCode == ENTER_KEY) {
+      console.log(`SEND REQUEST`);
+      return;
+    }
+    console.log(e.target.value);
+  }
+
   render() {
-    return `<input type="text" class="input-search" placeholder="Search for city or coordinates" />`;
+    return `<input 
+        id="search-field" 
+        type="text"
+        onKeyPress='${this.keyPressHandler}'
+        class="input-search" 
+        placeholder="Search for city or coordinates" 
+      />`;
   }
 }
